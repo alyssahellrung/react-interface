98679589a1a599bf780ddd7b5176ce272f4d7cb3
alyssahellrung <alyssahellrung@gmail.com>
2021-04-11 00:34:13 -0700

    added data file, subcomponents, state

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import AddAppointments from "./AddAppointments";
+import SearchAppointments from "./SearchAppointments";
+import ListAppointments from "./ListAppointments";
 import '../css/App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
   render() {
     return (
      <main className="page bg-white" id="petratings">
@@ -9,9 +33,9 @@ class App extends Component {
          <div className="row">
            <div className="col-md-12 bg-white">
              <div className="container">
-               <div>Add Appointments</div>
-               <div>Search Appointments</div>
-               <div>List Appointments</div>
+               <AddAppointments />
+               <SearchAppointments />
+               <ListAppointments />
              </div>
            </div>
          </div>
